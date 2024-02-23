539e9408f747a7379dffbdea3401f37213d4e03e
Juanita Velez <jvalentina.velez@gmail.com>
2024-02-23 06:36:02 -0500

    i18nPlural pipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -6,15 +6,37 @@ import { Component } from '@angular/core';
   styleUrl: './uncommon-page.component.css',
 })
 export class UncommonPageComponent {
-  //i18n select
+  //i18nSelect
   public name: string = 'Olivia';
   public gender: 'male' | 'female' = 'female';
   public invitationMap = {
     male: 'invitarlo',
     female: 'invitarla',
   };
-
   changeClient(): void {
     (this.name = 'Emilio'), (this.gender = 'male');
+  }
+
+  //i18nPlural
+  public clients: string[] = [
+    'Luna',
+    'Felix',
+    'Milo',
+    'Max',
+    'Oliver',
+    'Oreo',
+    'Mojito',
+    'Chewbacca',
+    'Bizcocho',
+    'Nemo',
+  ];
+  public clientsMap = {
+    '=0': 'no tenemos ningún gato esperando',
+    '=1': 'tenemos un gato esperando',
+    '=2': 'tenemos 2 gatos esperando',
+    other: 'tenemos # gatos esperando',
+  };
+  deleteClient(): void {
+    this.clients.pop();
   }
 }
