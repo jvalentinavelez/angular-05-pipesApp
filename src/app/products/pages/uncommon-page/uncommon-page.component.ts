203c535a27944854d3b5d08f38550628ceb99e1f
Juanita Velez <jvalentina.velez@gmail.com>
2024-02-23 06:25:05 -0500

    i18nSelectPipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -5,4 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './uncommon-page.component.html',
   styleUrl: './uncommon-page.component.css',
 })
-export class UncommonPageComponent {}
+export class UncommonPageComponent {
+  //i18n select
+  public name: string = 'Olivia';
+  public gender: 'male' | 'female' = 'female';
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
+
+  changeClient(): void {
+    (this.name = 'Emilio'), (this.gender = 'male');
+  }
+}
