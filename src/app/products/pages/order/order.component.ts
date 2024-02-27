913c465f23648c02872ac07518b8451dd075193d
Juanita Velez <jvalentina.velez@gmail.com>
2024-02-27 12:21:18 -0500

    añadir funcionalidad de sortBy a botones

--- src/app/products/pages/order/order.component.ts
+++ src/app/products/pages/order/order.component.ts
@@ -8,6 +8,7 @@ import { Color, Hero } from '../../interfaces/hero.interface';
 })
 export class OrderComponent {
   public isUpperCase: boolean = false;
+  public orderBy?: keyof Hero | undefined | '' = '';
   public heroes: Hero[] = [
     {
       name: 'Superman',
@@ -38,5 +39,9 @@ export class OrderComponent {
 
   toggleUpperCase(): void {
     this.isUpperCase = !this.isUpperCase;
+  }
+
+  changeOrder(value: keyof Hero) {
+    this.orderBy = value;
   }
 }
